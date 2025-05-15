c95902694917a32e30c22da8cdf1e7d41698422f
giuliagirotto <ggirotto17@gmail.com>
2025-05-15 16:30:28 +0200

    Create ModeratoreFrasi.java

---
+++ src/main/java/com/nonsense/Core/ModeratoreFrasi.java
@@ -0,0 +1,29 @@
+package com.nonsense.core;
+
+import com.google.cloud.language.v1.*;
+import com.google.cloud.language.v1.Document.Type;
+import com.nonsense.model.FraseNonSense;
+
+import java.io.IOException;
+
+public class ModeratoreFrasi {
+
+    public boolean valida(FraseNonSense frase) {
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+            Document doc = Document.newBuilder()
+                    .setContent(frase.toString())
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+
+            AnalyzeSentimentResponse response = language.analyzeSentiment(doc);
+
+            // Puoi anche usare classifyText o moderateText se disponibile
+            float score = response.getDocumentSentiment().getScore();
+
+            return score > -0.75; // se troppo negativo, la riteniamo tossica
+        } catch (IOException e) {
+            System.err.println("Errore nell'analisi della frase: " + e.getMessage());
+            return false; // meglio scartare in caso di errore
+        }
+    }
+}
