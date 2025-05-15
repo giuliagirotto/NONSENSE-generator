93cdbcbd6589612776ce717c3549b83c829efb46
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 17:27:20 +0200

    Update and rename Parola.java to Word.java

---
+++ src/main/java/com/nonsense/model/Word.java
@@ -0,0 +1,33 @@
+package com.nonsense.model;
+
+public class Word {
+    private final String text;
+    private final WordType type;
+
+    public Word(String text, WordType type) {
+        this.text = text;
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public TipoParola getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return text + " [" + type + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        Word word = (word) obj;
+        return text.equalsIgnoreCase(word.text) && type == word.type;
+    }
+}
