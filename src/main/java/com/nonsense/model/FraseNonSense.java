b69aeda9a2446d01f875a4ba8abedfe6ec8044be
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 16:51:34 +0200

    Update FraseNonSense.java

--- src/main/java/com/nonsense/model/FraseNonSense.java
+++ src/main/java/com/nonsense/model/FraseNonSense.java
@@ -1,18 +1,24 @@
 package com.nonsense.model;
 
-public class FraseNonSense {
-    private final Parola soggetto;
-    private final Parola verbo;
-    private final Parola oggetto;
+import java.util.*;
 
-    public FraseNonSense(Parola soggetto, Parola verbo, Parola oggetto) {
-        this.soggetto = soggetto;
-        this.verbo = verbo;
-        this.oggetto = oggetto;
+public class FraseNonSense {
+    private final List<Parola> parole;
+    
+    public FraseNonSense(List<Parola> parole) {
+        this.parole = parole;
+    }
+    
+    public List<Parola> getParole() {
+        treturn parole;
     }
 
     @Override
     public String toString() {
         return soggetto.getTesto() + " " + verbo.getTesto() + " " + oggetto.getTesto();
-    }
+
+        if (!frase.isEmpty()){
+            frase = frase.substring(0,1).toUpperCase() + frase.substring(1) + ".";
+        }
+    } 
 }
