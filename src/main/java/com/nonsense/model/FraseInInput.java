0a908a5db90ef11cd56d0184a37d76489565b6f9
giuliagirotto <ggirotto17@gmail.com>
2025-05-15 16:24:27 +0200

    Create FraseInInput.java

---
+++ src/main/java/com/nonsense/model/FraseInInput.java
@@ -0,0 +1,13 @@
+package com.nonsense.model;
+
+public class FraseInInput {
+    private final String testo;
+
+    public FraseInInput(String testo) {
+        this.testo = testo;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+}
