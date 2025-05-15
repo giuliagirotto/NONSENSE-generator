2614a8717871efb7352adbbc9c19f63d13486dff
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 16:31:22 +0200

    Update Main.java

--- src/main/java/com/nonsense/Main.java
+++ src/main/java/com/nonsense/Main.java
@@ -9,26 +9,36 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Dizionario dizionario = new Dizionario();
-        AnalizzatoreFrasi analizzatore = new AnalizzatoreFrasi();
-        Output output = new Output();
 
+        //ricevo frase in input
         System.out.println("Inserisci una frase:");
         String input = scanner.nextLine();
-
         FraseInInput frase = new FraseInInput(input);
+
+        //analizzo frase
+        AnalizzatoreFrasi analizzatore = new AnalizzatoreFrasi();
         List<Parola> parole = analizzatore.analizza(frase);
 
+        //creo un dizionario
+        Dizionario dizionario = new Dizionario();
         for (Parola p : parole) {
             dizionario.aggiungi(p);
         }
 
+        //richiedo quante frasi l'utente vuole generare  
         System.out.println("Quante frasi nonsense vuoi generare?");
         int n = Integer.parseInt(scanner.nextLine());
 
+        //genero frasi nonsense
         GeneratoreFrasi generatore = new GeneratoreFrasi(n);
-        List<FraseNonSense> frasi = generatore.genera(dizionario);
+        List<FraseNonSense> frasi = generatore.genera(parole, dizionario);
 
+        //controllo tossicità
+        ModeratoreFrasi moderatore = new ModeratoreFrasi();
+        frasi.removeIf(frase -> !moderatore.valida(frase));
+
+        //output
+        Output output = new Output();
         output.mostra(frasi);
     }
 }
