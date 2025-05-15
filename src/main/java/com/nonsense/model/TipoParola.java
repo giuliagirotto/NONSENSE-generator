939b28ac34afdd16bddef69e3a670443d95227bf
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 17:02:52 +0200

    Update TipoParola.java

--- src/main/java/com/nonsense/model/TipoParola.java
+++ src/main/java/com/nonsense/model/TipoParola.java
@@ -4,5 +4,16 @@ public enum TipoParola {
     NOME,
     VERBO,
     AGGETTIVO,
-    AVVERBIO
+    AVVERBIO;
+
+    //converte un tag grammaticale da API in TipoParola
+    public static TipoParola fromTag(String tag) {
+        switch (tag.toUpperCase()){
+            case "NOUN": return NOME;
+            case "VERB": return VERBO;
+            case "ADJECTIVE": return AGGETTIVO;    
+            case "ADVERB": return AVVERBIO;
+            default: return null;
+        }
+    }
 }
