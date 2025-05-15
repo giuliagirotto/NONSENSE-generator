49b995e2a8fdc15dfeff4298929e3b91b8c86742
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 17:28:39 +0200

    Update and rename TipoParola.java to WordType.java

---
+++ src/main/java/com/nonsense/model/WordType.java
@@ -0,0 +1,19 @@
+package com.nonsense.model;
+
+public enum WordType {
+    NOUN,
+    VERB,
+    ADJECTIVE,
+    ADVERB;
+
+    //converte un tag grammaticale da API in TipoParola
+    public static WordType fromTag(String tag) {
+        switch (tag.toUpperCase()){
+            case "NOUN": return NOUN;
+            case "VERB": return VERB;
+            case "ADJECTIVE": return ADJECTIVE;    
+            case "ADVERB": return ADVERB;
+            default: return null;
+        }
+    }
+}
