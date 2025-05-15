68ce0e3082cab872c4e613aee2f19692bd185dc1
giuliagirotto <ggirotto17@gmail.com>
2025-05-15 16:14:22 +0200

    Update Main.java

--- src/main/java/com/nonsense/Main.java
+++ src/main/java/com/nonsense/Main.java
@@ -1,1 +1,34 @@
-main
+package com.nonsense;
+
+import com.nonsense.core.*;
+import com.nonsense.io.Output;
+import com.nonsense.model.*;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Dizionario dizionario = new Dizionario();
+        AnalizzatoreFrasi analizzatore = new AnalizzatoreFrasi();
+        Output output = new Output();
+
+        System.out.println("Inserisci una frase:");
+        String input = scanner.nextLine();
+
+        FraseInInput frase = new FraseInInput(input);
+        List<Parola> parole = analizzatore.analizza(frase);
+
+        for (Parola p : parole) {
+            dizionario.aggiungi(p);
+        }
+
+        System.out.println("Quante frasi nonsense vuoi generare?");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        GeneratoreFrasi generatore = new GeneratoreFrasi(n);
+        List<FraseNonSense> frasi = generatore.genera(dizionario);
+
+        output.mostra(frasi);
+    }
+}
