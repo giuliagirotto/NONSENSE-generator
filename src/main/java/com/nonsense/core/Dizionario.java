49267a2d561111381aedd067705fe36891d9c1ec
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 16:41:03 +0200

    Update Dizionario.java

--- src/main/java/com/nonsense/core/Dizionario.java
+++ src/main/java/com/nonsense/core/Dizionario.java
@@ -5,14 +5,24 @@ import com.nonsense.model.TipoParola;
 import java.util.*;
 
 public class Dizionario {
-    private final List<Parola> parole = new ArrayList<>();
-
+    private final List<Parola> parole 
+    
+    public Dizionario (){
+        this.parole = new ArrayList<>();
+    }
+    //controlla se la parola è presente all'interno del dizionario
+    public boolean contiene(Parola parola){
+        return parole.contains(parola);
+    }
+    
+    //se la parola non è presente nel dizionario la aggiunge al suo interno
     public void aggiungi(Parola parola) {
         if (!parole.contains(parola)) {
             parole.add(parola);
         }
     }
 
+    //mostra tutte le parole di un tipo grammaticale specifico
     public List<Parola> getParolePerTipo(TipoParola tipo) {
         List<Parola> filtrate = new ArrayList<>();
         for (Parola p : parole) {
@@ -25,5 +35,10 @@ public class Dizionario {
 
     public List<Parola> getTutte() {
         return parole;
+    }
+
+    //restituisce il numero totale di parole nel dizionario
+    public int size(){
+        return parole.size();
     }
 }
