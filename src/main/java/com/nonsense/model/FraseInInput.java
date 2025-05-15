e5bd893b9727c7e08be920f3474c443eccd9ba16
silviamarcato <silviamarcatoo@gmail.com>
2025-05-15 17:07:28 +0200

    Update FraseInInput.java

--- src/main/java/com/nonsense/model/FraseInInput.java
+++ src/main/java/com/nonsense/model/FraseInInput.java
@@ -1,13 +1,22 @@
 package com.nonsense.model;
 
+//frase scritta dall'utente come input
 public class FraseInInput {
     private final String testo;
 
     public FraseInInput(String testo) {
+        if(testo == null || testo.trim().isEmpty()){
+            throw new IllegalArgumentException("Errore: il testo non può essere vuoto");
+        }
         this.testo = testo;
     }
 
     public String getTesto() {
+        return testo;
+    }
+
+    @Override
+    public String toString(){
         return testo;
     }
 }
