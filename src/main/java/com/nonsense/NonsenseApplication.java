23294766efe57db35400f4f33db08632d8ee4df5
Giacomo Tognon <giacomot277@gmail.com>
2025-05-19 16:40:35 +0200

    Update NonsenseApplication.java

--- src/main/java/com/nonsense/NonsenseApplication.java
+++ src/main/java/com/nonsense/NonsenseApplication.java
@@ -1,1 +1,11 @@
+package com.nonsense.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonsenseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NonsenseApplication.class, args);
+    }
+}
